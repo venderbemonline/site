d807d95d116ecb870d3ed48b7ffebd1153d2cd7c
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2025-06-23 15:03:06 +0000

    feat(site|vbo): add index and styles

---
+++ islands/HeroExample.tsx
@@ -0,0 +1,18 @@
+export function HeroExample() {
+  return (
+    <>
+      <img
+        class="my-6"
+        src="/logo.svg"
+        width="128"
+        height="128"
+        alt="the Fresh logo: a sliced lemon dripping with juice"
+      />
+      <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+      <p class="my-4">
+        Try updating this message in the
+        <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+      </p>
+    </>
+  );
+}
